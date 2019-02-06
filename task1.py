e808e23db11fc5fb5a7b321625161a6da5195678
Parth Patel <Parth.Patel@dal.ca>
2019-02-06 17:07:16 -0400

    subtask3 updated

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@ fout = open('Street_Centrelines.csv','r') #open and read file named StreetCenter
 # Easy: A tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
 
 def func_tuple():
+    #fout = open('Street_Centrelines.csv','r') #open and read file named StreetCenterlines.csv
     for item in fout:
         item = item.split(",")
         string = (item[2],item[4],item[6],item[7]) #item[2]=STR_NAME,item[4]=FULL_NAME,item[6]=FROM_STR,item[7]=TO_STR (columns in files)
@@ -11,6 +12,7 @@ def func_tuple():
 # A histogram of the different types of maintenance. 
         
 def maintanance_histogram():
+    #fout = open('Street_Centrelines.csv','r') #open and read file named StreetCenterlines.csv
     mydict = dict()
     for item in fout:
         item = item.split(",")
@@ -20,15 +22,18 @@ def maintanance_histogram():
             mydict[item[12]] += 1
     print(mydict)
 
-def unique_owner():
-    owner_list = item[11]
-    new_owner_list = []
+#List of unique owners for the streets ["OWN"]
+
+def owner_list():
+    fout = open('Street_Centrelines.csv','r') #open and read file named StreetCenterlines.csv
+    owners = []
     for item in fout:
         item = item.split(",")
-        if item[11] not in new_owner_list:
-            new_owner_list.append(item[11])
-    print(new_owner_list)
+        item = item[11].strip()
+        if item not in owners:
+            owners.append(item)
+    print(owners)
 
 func_tuple()
 maintanance_histogram()
-unique_owner()
+owner_list()
