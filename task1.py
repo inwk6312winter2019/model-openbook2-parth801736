d9486889ae0f117848f18582e7f1a45cfa57d719
Parth Patel <Parth.Patel@dal.ca>
2019-02-06 16:17:20 -0400

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,10 +5,10 @@ fout = open('Street_Centrelines.csv','r') #open and read file named StreetCenter
 def func_tuple():
     for item in fout:
         item = item.split(",")
-        string = (item[2],item[4],item[6],item[7])
+        string = (item[2],item[4],item[6],item[7]) #item[2]=STR_NAME,item[4]=FULL_NAME,item[6]=FROM_STR,item[7]=TO_STR (columns in files)
         print(string)  
 
-# A histogram of the different types of maintenance. [Result has to be a dictionary with key as Maintenance and number of streets as values] ["MAINTENANCE"]
+# A histogram of the different types of maintenance. 
         
 def maintanance_histogram():
     mydict = dict()
